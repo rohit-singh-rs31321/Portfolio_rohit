05453a2e2298fe6c699d91e294dbd09f0e997006
Rohit Singh <rs31321@gmail.com>
2024-03-20 16:37:42 +0530

    update leetcode link

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Social = () => {
   return (
    <div className="home__social">
-    <a href="https://leetcode.com/rohit4141/" className="home__social-icon" target="_blank">
+    <a href="https://leetcode.com/rohit-singh-rs31321/" className="home__social-icon" target="_blank">
     <i class='bx bx-code-block' ></i>    
     </a>
     <a href="https://www.linkedin.com/in/rohit-singh-rs31321/" className="home__social-icon" target="_blank">
