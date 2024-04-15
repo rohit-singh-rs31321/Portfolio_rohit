70f3ddb028cfdd89abcb6cb62cdf5fe381c28d5b
Rohit Singh <rs31321@gmail.com>
2024-04-15 22:46:57 +0530

    added new project

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,3 +1,4 @@
+import Work from "../../assets/work.png";
 import Work1 from "../../assets/work1.jpg";
 import Work2 from "../../assets/work2.jpg";
 import Work3 from "../../assets/work3.jpg";
@@ -7,36 +8,43 @@ import Work5 from "../../assets/work5.png";
 
 
 export const projectsData = [
-    {
-      id: 1,
+  {
+    id: 1,
+    image: Work,
+    title: "DrawWorks",
+    category: "web",
+    link: "https://drawworks.vercel.app/",
+  },
+  {
+      id: 2,
       image: Work1,
       title: "Volt",
       category: "web",
       link: "https://volt-myapp.netlify.app/",
     },
     {
-      id: 2,
+      id: 3,
       image: Work2,
       title: "ATM Simulator",
       category: "app",
       link: "https://github.com/rohit-singh-rs31321/Bank_Management_System/",
     },
     {
-      id: 3,
+      id: 4,
       image: Work3,
       title: "Mobiride",
       category: "design",
       link: "https://rohit-singh-rs31321.github.io/uber_clone/index.html",
     },
     {
-      id: 4,
+      id: 5,
       image: Work4,
       title: "Spotify Clone - Best of M C Square",
       category: "app",
       link: "https://spotify-rs.netlify.app/",
     },
     {
-      id: 5,
+      id: 6,
       image: Work5,
       title: "CODEASY - Online Code Editor",
       category: "web",
